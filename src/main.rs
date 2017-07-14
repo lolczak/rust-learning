4ee2da9928a609eb95e1543afd62c7474ef7b27e
lolczak <lukasz.jan.olczak@gmail.com>
2017-07-14 22:30:25 +0200

    chap2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+
+static MAX_HEALTH: i32 = 10;
+
+static GAME_NAME: &'static str = "Monster Attack";
+
 fn main() {
-    println!("Hello, world!");
+    const PI: f32 = 3.14;
+    println!("The game: {}", &GAME_NAME)
 }
