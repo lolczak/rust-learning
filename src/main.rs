b434d2b272051d1f29ba71610d4e9d59ee4a7659
lolczak <lukasz.jan.olczak@gmail.com>
2017-07-15 12:16:29 +0200

    chap2

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,5 @@ static GAME_NAME: &'static str = "Monster Attack";
 
 fn main() {
     const PI: f32 = 3.14;
-    println!("The game: {}", &GAME_NAME)
+    println!("The game: {}, {}", &GAME_NAME, PI)
 }
