05d36c1dd1756012e862b9c4a1419759a7c9758f
lolczak <lukasz.jan.olczak@gmail.com>
2017-07-30 12:50:54 +0200

    variables

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
+use std::f64::consts;
 
-static MAX_HEALTH: i32 = 10;
+static MAX_HEALTH: i32 = 1_000;
 
 static GAME_NAME: &'static str = "Monster Attack";
 
 fn main() {
     const PI: f32 = 3.14;
-    println!("The game: {}, {}", &GAME_NAME, PI)
+    println!("The game: {0}, {1}", &GAME_NAME, PI);
+    println!("Test pi: {:e}", consts::PI);
+    println!("The max health is equal: {max:b}", max = MAX_HEALTH);
+    foo();
+}
+
+fn foo() -> () {
+    let _bar = 5u32;
+    let pi = 3.14_f32;
+    let million = 1_000_000;
+    let empty = ();
+
+    return ();
 }
